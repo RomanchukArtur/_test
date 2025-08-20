60856db5e2e2668cb730b32392a0288b21aa2e56
RomanchukArtur <a.v.romanchuk@yandex.ru>
2025-08-20 21:51:23 +0300

    added Git/some_folder/

--- Git/gitLesson.py
+++ Git/gitLesson.py
@@ -1,1 +1,12 @@
-print('hello_world')
+# git add Git\gitLesson.py
+# git status
+# git config --global user.name "RomanchukArtur"
+# git config --global user.email "a.v.romanchuk@yandex.ru"
+# git config --list
+# git commit -m "initial commit"
+# git log
+
+print('hello_world_')
+
+# git remote add origin https://RomanchukArtur@github.com/RomanchukArtur/_test.git
+# git push -u origin master
